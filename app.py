24de5d0ac1961ffe62514a1d0868f81a20010fc3
saroltamarton <sarolta.marton@gmail.com>
2025-11-26 11:43:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 
+st.write("DEBUG: ESG app version 3")
+
 
 st.set_page_config(page_title="ESG Investment Portfolio Simulation",
                    page_icon="favicon.png",
